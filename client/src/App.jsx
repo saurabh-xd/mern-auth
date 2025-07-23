1319079b8cb5b054310a6c623e1917a430a955fb
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-07-23 16:24:36 +0530

    login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
+
 
 const App = () => {
   return (
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <img src={assets.logo} alt="" className='w-28 sm:w-32'/>
 
         <button onClick={()=>navigate('/login')}
-        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt="" /></button>
+        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all cursor-pointer'>Login <img src={assets.arrow_icon} alt="" /></button>
 
     </div>
   )
