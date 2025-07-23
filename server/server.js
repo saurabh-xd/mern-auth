15b0c67a1afde538d1089cec780ed242877c03dc
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-07-23 20:35:58 +0530

    connected backend with frontend

--- server/server.js
+++ server/server.js
@@ -9,9 +9,11 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
+const allowedOrigins = ['http://localhost:5173']
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}))
+app.use(cors({origin: allowedOrigins ,credentials: true}))
 
 app.get('/', (req,res)=> res.send("hello"))
 
