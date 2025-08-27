be317c95541690503b101c2e90abed5e0e7f789a
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-08-27 20:04:15 +0530

    update allowedOrigins to use production URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://mern-auth-vbcd.onrender.com']
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
